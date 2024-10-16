200647db9509b6714d84de0f2bb7bda6f5cc86ea
Asaad Hussain <asaadhussain2014@gmail.com>
2024-10-16 13:23:23 +0500

    Created Schema and Model

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 
+import express from 'express';
+import mongoose from 'mongoose';
 
-export function CRUD() {
-    console.log("Hello");
-}
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("From the app");
+});
+
+mongoose.connect("mongodb://localhost:27017/")
+    .then(() => {
+        console.log("Connection succesful");
+        app.listen(3000, () => {
+            console.log("Listening to port 3000")
+        });
+    }).catch((error) => {
+        console.log("Failed to connect ", error);
+    });
